ca2137722f3a4f3dafd02904a6edd4e251b22dbf
Nirvair Sangha <nirvairsangha13@gmail.com>
2020-11-07 17:43:15 -0600

    Tools

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 //Import components that you create here
 import Explanation from './Explanation/Explanation'
+import Tools from './Tools/Tools'
 
 function LandingPage() {
     return (
         //Use components that you import here
         <>
             <Explanation />
+            <Tools />
         </>
     )
 }
---
+++ frontend/src/components/LandingPage/Tools/Tools.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Jumbotron, Media } from 'reactstrap';
+import ReactPicture from './Pictures/ReactPicture.png'
+import NodePicture from './Pictures/NodePicture.png'
+import JiraPicture from './Pictures/JiraPicture.png'
+import GithubPic from './Pictures/GithubPic.png'
+
+const Tools = (props) => {
+    return (
+        <div>
+            <Jumbotron>
+                <h1 className="display-3">Tools</h1>
+                <p className="lead">Some of the tools we will be using to work on this project include React.js to develop the 
+                front end, node.js to develop the back end, Jira to help keep track of what tasks must be done by who and when,
+                and finally GitHub which will simplify the process of working as a team and keeping track of multiple changes in the code.</p>
+                <hr classname="my-2" />
+                <Media>
+                    <Media left top href="#">
+                        <Media object src={ReactPicture} alt="" />
+                    </Media>
+                    <Media>
+                        <Media left middle href="#">
+                            <Media object src={NodePicture} alt="" />
+                        </Media>
+                    </Media>
+                    <Media>
+                        <Media left bottom href="#">
+                            <Media object src={JiraPicture} alt="" />
+                        </Media>
+                    </Media>
+                    <Media>
+                        <Media right bottom href="#">
+                            <Media object src={GithubPic} alt=""/>
+                        </Media>
+                    </Media>
+                </Media>
+            </Jumbotron>
+        </div>
+    );
+};
+
+export default Tools;
