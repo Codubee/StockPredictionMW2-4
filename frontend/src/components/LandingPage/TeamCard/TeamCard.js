10e3ac0dddcf6817059fe78510087936b9d535ac
Maaha Sakhia <maahasakhia@gmail.com>
2020-11-11 14:44:29 -0600

    team card component

--- frontend/src/components/LandingPage/TeamCard/TeamCard.js
+++ frontend/src/components/LandingPage/TeamCard/TeamCard.js
@@ -15,9 +15,9 @@ const TeamCard = (props) => {
         <Image className = "headshot" src = {props.headshot} alt = {props.name}  roundedCircle/>
         <CardBody className="text-center">
           <CardTitle tag="h5">{props.name}</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">{props.line1}<br />{props.line2}<br />{props.line3}<br /></CardSubtitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">{props.line1}<br />{props.line2}<br />{props.line3}</CardSubtitle>
           {/*<CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>*/}
-           <br /><a target = "_blank" rel="noreferrer" href = {props.github}><img height = "40px" src = "/githubLogo.png"/></a>
+           <hr /><a target = "_blank" rel="noreferrer" href = {props.github}><img height = "40px" src = "/githubLogo.png"/></a>
         </CardBody>
       </Card>
       <br />
