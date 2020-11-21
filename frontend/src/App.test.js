236fd89ade05e76e1d471ec4522077bc58904f18
Sunny Guan <sunny.peng.guan@gmail.com>
2020-11-21 00:04:12 -0600

    CB-7-Router (#4)
    * added routing and placeholder for user input
    
    * updated tests to use shallow instead of mount

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,15 +2,32 @@ import App from './App';
 import MyNavbar from "./components/MyNavbar/MyNavbar";
 import { shallow } from "enzyme";
 import { hasBeenRendered } from "./global";
+import { Route } from 'react-router';
+import SamplePage from './components/SamplePage/SamplePage';
+import LandingPage from './components/LandingPage/LandingPage';
 
 const wrapper = shallow(<App />);
 
+const pathMap = wrapper.find(Route).reduce((pathMap, route) => {
+  const routeProps = route.props();
+  pathMap[routeProps.path] = routeProps.component;
+  return pathMap;
+}, {});
+
 describe("Test App Component", () => {
   it("should render", () => {
-      hasBeenRendered(wrapper);
+    hasBeenRendered(wrapper);
   });
 
   it("should render navbar", () => {
-      hasBeenRendered(wrapper.find(MyNavbar));
+    hasBeenRendered(wrapper.find(MyNavbar));
+  });
+
+  it('renders input page', () => {
+    expect(pathMap['/input']).toBe(SamplePage);
+  });
+
+  it('renders landing page', () => {
+    expect(pathMap['/']).toBe(LandingPage);
   });
 });
---
+++ frontend/src/components/SamplePage/SamplePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SamplePage() {
+  return (
+    <p>Placeholder for routing</p>
+  );
+};
+
+export default SamplePage;
