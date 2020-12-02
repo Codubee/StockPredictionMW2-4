214b76cb311a20ea083458208c53f23af016f541
Maaha Sakhia <maahasakhia@gmail.com>
2020-12-01 22:08:42 -0600

    StyleSheet

---
+++ frontend/src/components/StyleSheet/StyleSheet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './StyleSheet.css'
+
+function StyleSheet() {
+    return (
+        <div>
+        <h1>StyleSheet</h1>
+    </div>
+    )
+}
+
+export default StyleSheet
