60e2403ff482e04b8f99206e0066569b1fd1921c
Maaha Sakhia <maahasakhia@gmail.com>
2020-12-09 18:54:32 -0600

    efficiency of code -- changes requested

--- frontend/src/components/LandingPage/Weather/Weather.js
+++ frontend/src/components/LandingPage/Weather/Weather.js
@@ -7,17 +7,16 @@ const weatherData = {
     picture: "/partly cloudy.png"
 };
 
-class Weather extends React.Component {
-    render() {
+
+function Weather() {
+        const { city, temperature, picture } = weatherData;
         return(
             <span>
-               <b>{weatherData.city}</b>
-               {weatherData.temperature}
-               <img className= "medium-icon" src = {weatherData.picture} />
+               <b>{city}</b>
+               {temperature}
+               <img className="medium-icon" src={picture} />
             </span>
         );
-    }
-
 }
 
 export default Weather;
