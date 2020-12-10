28edce01412ae1903123947c75866e3d6113ab5e
Sunny Guan <sunny.peng.guan@gmail.com>
2020-12-09 16:53:42 -0800

    added endpoint for getting news

--- backend/index.js
+++ backend/index.js
@@ -14,4 +14,23 @@ app.listen(port,()=>{
     console.log('API is up and running')
 })
 
+app.get('/getNews', (req, res) => {
+    var company = req.query.company;
+    var today = new Date();
+    var weekAgo = new Date();
+    weekAgo.setDate(today.getDate() - 7);
+
+    var from = weekAgo.toISOString().split("T")[0];
+    var to = today.toISOString().split("T")[0];
+
+    axios.get(`https://finnhub.io/api/v1/company-news?symbol=${company}&from=${from}&to=${to}`,
+                {"X-Finnhub-Token": process.env.FINNHUB_TOKEN})
+        .then(response => {
+            res.status(200).json(response.data);
+        }).catch((error)=>{
+            console.log(error);
+            res.status(400).json({error:"Error occurred!"});
+        })
+})
+
 module.exports = app;
