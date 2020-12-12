ef1467ecb28e2d7b4901d78219eed05c4f83350d
DHua5922 <hua.dylan@gmail.com>
2020-12-12 16:15:54 -0600

    Created API for getting stock quote

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
 require('dotenv').config();
 app.use(express.json())
 
 const port = 8080;
+
+const finnhub = require('finnhub');
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = process.env.API_KEY;
+const finnhubClient = new finnhub.DefaultApi();
 
 app.get('/',(req,res)=>{
     res.send('Hello world');
@@ -13,5 +17,13 @@ app.get('/',(req,res)=>{
 app.listen(port,()=>{
     console.log('API is up and running')
 })
+
+//Quote
+app.get('/quote', (req, res) => {
+    finnhubClient.quote("AAPL", (error, data, response) => {
+        if(error) res.status(500).send(error);
+        else res.status(200).send(data);
+    });
+});
 
 module.exports = app;
