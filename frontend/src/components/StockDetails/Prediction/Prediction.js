419cfd86ae5e4810d9db3f721d7b635c8dcb477b
Miguel <info@codubee.com>
2020-12-16 12:25:07 -0600

    Removed unused comments

--- frontend/src/components/StockDetails/Prediction/Prediction.js
+++ frontend/src/components/StockDetails/Prediction/Prediction.js
@@ -8,21 +8,6 @@ export default class Prediction extends React.Component{
 
     componentDidMount(){
         //here we call the comprehend api
-        // fetch("/getStockPrice",{
-        //     method: 'POST', 
-        //     headers: {
-        //       'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(data),
-        //   })
-        //     .then(res => res.json())
-        //     .then(data => {
-
-        //         this.setState({ lat: data.lat, lng: data.lng, }
-        //             ,() => this.props.parentData(this.state))
-        //         console.log(data);
-        //     })
-        //     .catch(console.log)
     }
 
     render(){
