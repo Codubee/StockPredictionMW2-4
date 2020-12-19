83542cd77a7d8c4b6bde55e89c9c54d1a2afb719
Dylan Hua <hua.dylan@gmail.com>
2020-12-19 12:54:22 -0600

    CB-21-stock-api (#11)
    * Created API for getting stock quote
    
    * Renamed API key and added url parameter

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
 require('dotenv').config();
 app.use(express.json())
 
 const port = 8080;
+
+const finnhub = require('finnhub');
+const finnhub_api_key = finnhub.ApiClient.instance.authentications['api_key'];
+finnhub_api_key.apiKey = process.env.FINNHUB_API_KEY;
+const finnhubClient = new finnhub.DefaultApi();
 
 app.get('/',(req,res)=>{
     res.send('Hello world');
@@ -14,6 +18,14 @@ app.listen(port,()=>{
     console.log('API is up and running')
 })
 
+//Quote
+app.get('/quote', (req, res) => {
+    var company = req.query.company;
+    finnhubClient.quote(company, (error, data, response) => {
+        if(error) res.status(500).send(error);
+        else res.status(200).send(data);
+    });
+});
 app.get('/getNews', (req, res) => {
     var company = req.query.company;
     var today = new Date();
--- backend/test/index_test.js
+++ backend/test/index_test.js
@@ -1,15 +1,25 @@
+const { expect } = require('chai');
 let chai = require('chai')
 let chaiHttp = require('chai-http')
 let app =  require('../index')
 chai.should();
 chai.use(chaiHttp);
 
-describe('Tests get enpoint at /',()=>{
-    it('Should return hello world',(done)=>{
+describe('Tests enpoint at index.js',()=>{
+    it('/ should return hello world',(done)=>{
         chai.request(app)
         .get('/')
         .end((err,res)=>{
             res.text.should.be.eql('Hello world')
+            done();
+        })
+    })
+
+    it('/quote should return stock quote',(done)=>{
+        chai.request(app)
+        .get('/quote?company=AAPL')
+        .end((err,res)=>{
+            expect(res).to.have.status(200);
             done();
         })
     })
