4a6e034377b97eb3d5ecdc041bca987d6d10228e
Nirvair Sangha <56703837+Nirvair-Sangha@users.noreply.github.com>
2020-12-27 14:17:15 -0600

    Cb 11 aws comprehend api (#17)
    * body-parser
    
    * test and post req
    
    * changes to formatting
    
    * removed node_modules from root dir
    
    Co-authored-by: Sunny Guan <sunny.peng.guan@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+
 require('dotenv').config();
 app.use(express.json())
 
@@ -12,6 +14,22 @@ const finnhubClient = new finnhub.DefaultApi();
 
 app.get('/',(req,res)=>{
     res.send('Hello world');
+})
+
+app.post('/checkNews', function (req, res) {
+    const body = {
+        news: req.body
+    }
+
+    axios.post('https://codubee-api.herokuapp.com/comprehension',body)
+        .then(function (response) {
+            console.log(response.data);
+            res.status(200).json(response.data);
+        })
+        .catch(function (error) {
+            console.log(error)
+            res.status(400).json({error:"An error occurred"});
+        })
 })
 
 app.listen(port,()=>{
--- backend/test/index_test.js
+++ backend/test/index_test.js
@@ -8,11 +8,21 @@ chai.use(chaiHttp);
 describe('Tests enpoint at index.js',()=>{
     it('/ should return hello world',(done)=>{
         chai.request(app)
-        .get('/')
-        .end((err,res)=>{
-            res.text.should.be.eql('Hello world')
-            done();
-        })
+            .get('/')
+            .end((err,res)=>{
+                res.text.should.be.eql('Hello world')
+                done();
+            })
+    })
+
+    it('Should send paramters to /checkNews',(done)=>{
+        chai.request(app)
+            .post('/checkNews')
+            .send({news: ['test', 'other']})
+            .end((err, res)=>{
+                expect(res).to.have.status(200);
+                done();
+            })
     })
 
     it('/quote should return stock quote',(done)=>{
